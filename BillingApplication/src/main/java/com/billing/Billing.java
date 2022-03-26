d73b2188e9f05ae461e09581e9ec7f6f4042ec83
sharansantosh97 <sharan.santosh.ss@gmail.com>
2022-03-26 16:13:26 -0700

    Setting up maven project structure

---
+++ BillingApplication/src/main/java/com/billing/Billing.java
@@ -0,0 +1,22 @@
+package com.billing;
+
+
+
+
+public class Billing
+{
+	
+    public static void main(String args[])
+    {
+    	
+    	System.out.println("test sample");
+    }
+    
+    
+}
+    
+    
+    
+   
+
+
