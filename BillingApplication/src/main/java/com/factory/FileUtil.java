a35d562a2394f0c0ff6fc39c79bf477288c2ab75
sharansantosh97 <sharan.santosh.ss@gmail.com>
2022-04-10 21:26:56 -0700

    Added File Utility class for reading files

---
+++ BillingApplication/src/main/java/com/factory/FileUtil.java
@@ -0,0 +1,43 @@
+package com.factory;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import com.modelclasses.BillingItem;
+
+public class FileUtil {
+    Path filePath;
+    
+    private ArrayList<String> fileContent = new ArrayList<String>();
+
+    public FileUtil(String filePath) {
+        this.filePath = Paths.get(filePath);
+    }
+
+    public void reader(boolean ignoreFirst) throws IOException {
+        if (Files.notExists(filePath)) {
+            new IOException();
+        }
+        BufferedReader br = new BufferedReader(new FileReader(filePath.toFile()));
+        String contentLine = "";
+        while ((contentLine = br.readLine()) != null) {
+            if (ignoreFirst) {
+                ignoreFirst = false;
+                continue;
+            }
+            fileContent.add(contentLine);
+
+        }
+    }
+
+    public ArrayList<String> getFileContent() {
+        return fileContent;
+    }
+
+    
+}
