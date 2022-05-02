f5675140d6bbc9b8eeaf0524be47fddcb77398e0
sharansantosh97 <sharan.santosh.ss@gmail.com>
2022-05-01 19:45:27 -0700

    bugs fixed

--- BillingApplication/src/main/java/com/billing/Billing.java
+++ BillingApplication/src/main/java/com/billing/Billing.java
@@ -1,5 +1,8 @@
 package com.billing;
 
+
+
+import java.io.IOException;
 import java.util.Scanner;
 
 import com.database.StaticDatabase;
@@ -7,45 +10,45 @@ import com.database.StaticDatabase;
 public class Billing
 {
 	
-    public static void main(String args[])
+    public static void main(String args[])throws IOException
     {
-    	
     	StaticDatabase db = StaticDatabase.getInstance();
     	db.loadDatabase();
-    	
     	System.out.println("Database loaded with the required data");
 
-    	Scanner sc=new Scanner(System.in).useDelimiter("\n");
-        System.out.println("Please enter the input file path for placing the order");
-        String fPath="";
-        try{
-        	fPath=sc.next();
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-        Orders o=new Orders(fPath);
-        if(o.startOrder()==true)
-        {
-        	System.out.println("Processing your order");
-            if(o.verifyOrderDetails()==true) 
+        	Scanner sc=new Scanner(System.in).useDelimiter("\n");
+            System.out.println("Please enter the input file path for placing the order");
+            String fPath="";
+            try{
+            	fPath=sc.next();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            
+            Orders o=new Orders(fPath);
+            if(o.startOrder()==true)
             {
-                System.out.println("All Products are available in stock and under each category limit");
-                o.getTotalPrice();
-                o.checkoutOrder();
-                System.out.println("Success! Order has been placed.");
-                System.out.println("Please refer to successFile under Output folder of the project structure for more details");
+                System.out.println("Processing your order");
+                if(o.verifyOrderDetails()==true) 
+                {
+                    System.out.println("All Products are available in stock and under each category limit");
+                    o.getTotalPrice();
+                    o.checkoutOrder();
+                    System.out.println("Success! Order has been placed.");
+                    System.out.println("Please refer to successFile under Output folder of the project structure for more details");
 
+                }
+                else
+                {
+                        System.out.println("Error Occured, please check error file under Output folder of the project structure for more details");
+                        o.showOutput();
+                }
             }
             else
             {
-                    
+                System.out.println("Please enter correct inuput file path");
             }
-        }
-        else
-        {
-            System.out.println("Please enter correct inuput file path");
-        }
-    }
+     }
     
     
 }
